74b919d40b5120365c1ad671fec22fb9c06f1eaf
Dmitrii Romanenko <dmitriiromanenko@Dmitrii--Notebook.local>
2025-10-24 18:19:35 +0200

    small changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: `./`,
+  base: `/standards_calculator/`,
   css: {
     postcss: {
       plugins: [tailwindcss()],
