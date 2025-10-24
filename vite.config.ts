6dbdfab6ecc2f6e559b2cb0cf8942b91e472141b
Dmitrii Romanenko <dmitriiromanenko@Dmitrii--Notebook.local>
2025-10-24 18:05:03 +0200

    Fix Vite base path for GitHub Pages 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import react from "@vitejs/plugin-react"
+import path from "path"
 import tailwindcss from "tailwindcss"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/standards_calculator/",
+  base: `/standards_calculator`,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
+  },
+  server: {
+    port: 5173,
   },
 })
